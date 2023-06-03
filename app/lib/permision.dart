1eb89f863b3598aea4962a1f9b64eff057e1f5d6
gukkame <guka245@gmail.com>
2023-06-03 17:02:47 +0300

    set up folders

---
+++ app/lib/permision.dart
@@ -0,0 +1,14 @@
+import 'package:geolocator/geolocator.dart';
+
+class Permision {
+// requests permisions and return current position
+  Future<Position> getUserCurrentLocation() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) async {
+      await Geolocator.requestPermission();
+      print("ERROR" + error.toString());
+    });
+    return await Geolocator.getCurrentPosition();
+  }
+}
